97cf8a3ca95c98720c5ccf20e7731396958dd4c7
Adam Blum <adam@dplrn.com>
2023-11-02 17:37:34 +0000

    refactor code and use DiskANN index on Milvus

--- skills_for_jobs.py
+++ skills_for_jobs.py
@@ -50,9 +50,11 @@ def save_job_skills_pinecone(job_skills,filename):
 def save_job_skills_weaviate(job_skills,filename):
     rows=[]
     tot_quality=0
+    #print(f"Looping through {len(job_skills)} items")
     for key,job in job_skills.items():
         count=0
         row = {"job":key}
+        #print(f"Job {job}")
         skills=job['data']['Get']['Skill']
         i=0
         prev_skill = None
@@ -71,6 +73,7 @@ def save_job_skills_weaviate(job_skills,filename):
     df = pd.DataFrame(rows)
     df.to_csv(filename)
 
+import json 
 def save_job_skills_milvus(job_skills,filename):
     rows=[]
     tot_quality = 0
@@ -79,11 +82,17 @@ def save_job_skills_milvus(job_skills,filename):
         i=0
         count = 0 
         #print(f"Job: {job}")
-        for id in job.ids:
-            value = row["skill"+str(i+1)] = id
-            i+=1
-            if job_has_skill(key,value):
-                count += 1
+        if job is None:
+            break 
+        for hit in job:
+            if hit is None: 
+                break
+            if hit: 
+                #print(f"Hit {hit}")
+                value = row["skill"+str(i+1)] = hit.id
+                i+=1
+                if job_has_skill(key,value):
+                    count += 1
         row["quality"] = count 
         tot_quality += count 
         rows.append(row)
@@ -93,70 +102,53 @@ def save_job_skills_milvus(job_skills,filename):
     df.to_csv(filename)
 
 
-pinecone.api_key = os.environ['PINECONE_API_KEY']
-skill_index_name = 'skills'
-job_index_name = 'jobs'
-env = os.environ['PINECONE_ENV']
+def init_pinecone():
+    pinecone.api_key = os.environ['PINECONE_API_KEY']
+    skill_index_name = 'skills'
+    env = os.environ['PINECONE_ENV']
+    pinecone.init(api_key=pinecone.api_key, environment = env)
+    index = pinecone.Index(skill_index_name)
+    return index
 
-pinecone.init(api_key=pinecone.api_key, environment = env)
-pinecone_skills_index = pinecone.Index('skills')
-
-resource_owner_config = weaviate.AuthClientPassword(
+def init_weaviate():
+    resource_owner_config = weaviate.AuthClientPassword(
         username = os.environ['WEAVIATE_USER'],
         password = os.environ['WEAVIATE_PASSWORD'],
         scope = "offline_access" # optional, depends on the configuration of your identity provider (not required with WCS)
     )
-
-weaviate_client = weaviate.Client(
+    client = weaviate.Client(
         url = WEAVIATE_SERVER,  
         auth_client_secret=resource_owner_config
     )
+    return client 
 
-token = os.environ['MILVUS_API_KEY']
-uri = os.environ['MILVUS_URL']
-token = "db_admin:"+ os.environ['MILVUS_PASSWORD']
-uri = os.environ['MILVUS_URL']
-connections.connect("default", uri=uri, token=token)
-# TODO: create the Milvus index via API if it doesn't exist yet
+def init_milvus():
+    token = os.environ['MILVUS_API_KEY']
+    uri = os.environ['MILVUS_URL']
+    token = "db_admin:"+ os.environ['MILVUS_PASSWORD']
+    uri = os.environ['MILVUS_URL']
+    connections.connect("default", uri=uri, token=token)
+    collection = Collection("skills")
+    return collection
 
-skills_collection = Collection("skills")
-
-labeled_job_skills = pd.read_csv('./data/labeled_job_skills.csv')
-
-jobs_path = './data/job_title_desc.csv'
-jobs_df = pd.read_csv(jobs_path)
-if (jobs_df.size == 0) or (jobs_df[jobs_df['org_name']==TARGET_ORG].size==0):
-    print ("No jobs are available.")
-    exit()
-jobs_vectors = np.load('./data/jd_sem_vec.npy')
-
-job_skills_pinecone = {}
-job_skills_weaviate = {}
-job_skills_milvus = {}
-tot_durations = {'pinecone':0,'weaviate':0,'milvus':0,'pg':'0'}
-
-for i, job in jobs_df.iterrows():
-    if job['org_name']!=TARGET_ORG: 
-        continue
-    if len(job_skills_pinecone)>=MAX_JOBS:
-        break
-    job_vec = jobs_vectors[i]
-    ######################
-    # pinecone search
-    #print(f"Finding skills for job {job.loc['job_title']}")
+def pinecone_search(index,job_vec):
     start = time.time()
-    result = pinecone_skills_index.query(vector=job_vec.tolist(),top_k=MAX_SKILLS*5,include_values=True)
+    result = index.query(vector=job_vec.tolist(),top_k=MAX_SKILLS*5,include_values=True)
     #print(f"Result {result}")
     end = time.time()
     duration = end - start
+
     job_skills_pinecone[job['job_title']]=result
     print (f"Query time Pinecone: {duration} seconds")
     tot_durations['pinecone'] += duration 
-    ##############################
-    # weaviate search
+    return result
+
+def weaviate_search(client,job_vec):
+    if len(job_skills_weaviate)>=MAX_JOBS:
+        return None 
     start = time.time()
     result = (
-        weaviate_client.query
+        client.query
         .get("Skill", ["abbreviation", "title"])
         .with_near_vector({
             "vector": job_vec
@@ -165,23 +157,21 @@ for i, job in jobs_df.iterrows():
         .with_additional(["distance"])
         .do()
     )
-    #print(f"Result {result}")
     end = time.time()
     duration = end - start
     job_skills_weaviate[job['job_title']]=result
     print (f"Query time Weaviate: {duration} seconds")
     tot_durations['weaviate'] += duration 
+    return result
 
-    ##############################
-    # milvus search 
+def milvus_search(collection,job_vec):
+    if len(job_skills_weaviate)>=MAX_JOBS:
+        return None 
     search_params = {
         "metric_type": "L2", 
-        "offset": 5, 
-        "ignore_growing": False, 
-        "params": {"nprobe": 10}
     }
     start = time.time()
-    milvus_result = skills_collection.search(
+    results = collection.search(
         data=[job_vec], 
         anns_field="embeddings",
         # the sum of `offset` in `param` and `limit` 
@@ -196,12 +186,39 @@ for i, job in jobs_df.iterrows():
     ) 
     end = time.time()
     duration = end - start
-    job_skills_milvus[job['job_title']]=milvus_result[0]
+    result=results[0]
     print (f"Query time Milvus: {duration} seconds")
     tot_durations['milvus'] += duration 
+    #print(f"Milvus result {results}")
+    return result 
 
+jobs_path = './data/job_title_desc.csv'
+jobs_df = pd.read_csv(jobs_path)
+if (jobs_df.size == 0) or (jobs_df[jobs_df['org_name']==TARGET_ORG].size==0):
+    print ("No jobs are available.")
+    exit()
+jobs_vectors = np.load('./data/jd_sem_vec.npy')
 
-# report on the search times
+job_skills_pinecone = {}
+job_skills_weaviate = {}
+job_skills_milvus = {}
+tot_durations = {'pinecone':0,'weaviate':0,'milvus':0,'pg':'0'}
+
+pinecone_skills_index = init_pinecone() 
+weaviate_client = init_weaviate()
+milvus_collection = init_milvus()
+for i, job in jobs_df.iterrows():
+    if job['org_name']!=TARGET_ORG: 
+        continue
+    job_vec = jobs_vectors[i]
+    if len(job_skills_pinecone)>=MAX_JOBS:
+        break
+    job_skills_pinecone[job['job_title']] = pinecone_search(pinecone_skills_index,job_vec)
+    job_skills_weaviate[job['job_title']] = weaviate_search(weaviate_client,job_vec)
+    job_skills_milvus[job['job_title']] = milvus_search(milvus_collection,job_vec)
+
+# save the job skills alignments and report on the search times and quality
+labeled_job_skills = pd.read_csv('./data/labeled_job_skills.csv')
 avg_query_time = tot_durations['pinecone'] / len(job_skills_pinecone)
 print(f"Total query time {tot_durations['pinecone']}, average {avg_query_time}")
 save_job_skills_pinecone(job_skills_pinecone,'job_skills_pinecone.csv')
